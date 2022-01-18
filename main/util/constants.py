59755a50be95efc4260e762f33afa71afb947153
leahfortier <leahfortier318@gmail.com>
2022-01-17 16:29:32 -0800

    '01/31/2019 Any% (1)' update!

--- main/util/constants.py
+++ main/util/constants.py
@@ -4,7 +4,8 @@ from main.util.file_io import with_path
 SPREADSHEET_ID = '1Sj8JsgVGipsAsHq5V4AIrLnBf_vzHmUbSWN2WnZAiZI'
 BOARD_TAB = 'Scoreboard'
 SHOWDOWN_TABS = [
-    '08/12/2018 Any%'
+    '08/12/2018 Any%',
+    '01/31/2019 Any% (1)',
 ]
 
 MODE = 'Mode'
